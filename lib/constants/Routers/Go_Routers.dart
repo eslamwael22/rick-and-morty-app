d82b8a37a67dafa7cccb86767632debf38391e71
Eslam Wael <eldasheslam105@gmail.com>
2026-07-04 16:42:34 +0300

    Use Go_router pakage

---
+++ lib/constants/Routers/Go_Routers.dart
@@ -0,0 +1,13 @@
+import 'package:films_project/presentation/screens/charcters_screen.dart';
+import 'package:films_project/presentation/screens/details_characters_screen.dart';
+import 'package:go_router/go_router.dart';
+
+final GoRouter router = GoRouter(
+  routes: [
+    GoRoute(path: '/', builder: (context, state) => const CharctersScreen()),
+    GoRoute(
+      path: '/details',
+      builder: (context, state) => const DetailsCharactersScreen(),
+    ),
+  ],
+);
---
+++ lib/presentation/screens/charcters_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CharctersScreen extends StatelessWidget {
+  const CharctersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('Charcters Screen')));
+  }
+}
